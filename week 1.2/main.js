ceecf22fd2fe0637c679977fd8fa6dc4a2dfff73
Prashant Kumar Singh <99783341+prashantkumarsingh07@users.noreply.github.com>
2024-03-10 23:41:23 +0530

    Add files via upload

---
+++ week 1.2/main.js
@@ -0,0 +1,117 @@
+
+// for(let  i=1 ; i<=1000;i++)
+// {
+//   console.log(i);
+// }
+
+// # VARIABLES in JS
+
+// var a =1;
+// console.log(a);
+
+// var a=1;
+// a=2;
+// console.log(a); op-2
+
+// let a=1;
+// a=2;
+// console.log(a); //op - 2
+
+// const a = 1;
+// a=2;
+// console.log(a);  // type error
+
+// # DATA TYPES IN JS
+
+//  let name ="Prashant kumar Singh";
+//  let firstname ="Prashant kumar Singh";
+//  let age=21;
+//  let isMarried=false;
+
+//  console.log("This persons name is "+ name + "his age is"+ age) // name is depreciated
+
+//  console.log("This persons name is "+ firstname + "his age is "+ age); 
+
+
+// #  IF ELSE STATEMENTS
+
+// let firstname ="Prashant kumar Singh";
+//   let age=21;
+//   let isMarried=false;
+//   if( isMarried )
+//   console.log("married");
+// else
+// console.log("not married");
+
+
+//  # LOOPS
+
+// Program to print 1 to 1000
+
+
+// for( let i=1; i<=1000;i++)
+// console.log(i);
+
+// Program to print sum from 1 to 1000;
+
+// let ans=0;
+// for(let i=1;i<=1000;i++)
+// {
+//   ans+=i;
+// }
+// console.log(ans);
+
+// wap to greet a person given their first name and last name
+
+// let firstname="Prashant";
+// let lastname="kumar";
+// console.log("hello "+firstname +" "+lastname + " welcome to kirat cohort");
+
+// # COMPLEX PRIMITIVES #//
+
+//  ARRAYS & OBJECTS
+
+// wap to print all the even nos in an array
+
+// let arr = [1,2,3,4,5,6];
+// let s=0;
+// for(let a=0;a<arr.length;a++)
+// {
+//   s=arr[a];
+//   if(s%2 ==0)
+//   console.log(s);
+// }
+
+//  wap to build biggest number in an array
+  
+  //  let arr=[2,5,6,90,67,100];
+  //  let s=0;
+  //  let greater=0;
+  //  for(let a=0;a<=arr.length;a++)
+  //  {
+  //       s=arr[a];
+  //       if(s>greater)
+  //       greater=s;
+  //  }
+  //  console.log(greater);
+
+  // wap that reverse al the elements in an array
+
+  // let arr=[1,2,3,4,5];
+  
+  // let temp=0;
+  // let left=0;
+  // let right=arr.length-1;
+  
+  
+  // while(left<right)
+  // {
+   
+  //   temp=arr[right];
+  //   arr[right]=arr[left];
+  //   arr[left]=temp;
+  //   left++;
+  //   right--;
+
+  // }
+  // console.log(arr);
